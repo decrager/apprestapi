796797cae365c50ec08f54fd86e9d9802fda2c00
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-01-21 10:00:11 +0700

    Routes setting Server,js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 //Parse Application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//Call Routes
+var routes = require('./routes');
+routes(app);
+
  app.listen(3000, () => {
      console.log(`Server started on port`)
  });
