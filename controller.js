39df808ca2522e25ff3233e6f66271e66e403500
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-01-21 10:36:49 +0700

    Get Data Works

--- controller.js
+++ controller.js
@@ -2,7 +2,19 @@
 
 var response = require('./rest');
 var connection = require('./koneksi');
+const Connection = require('mysql/lib/Connection');
 
 exports.index = function(req, res){
-    response.ok("REST API Running")
-}
+    response.ok("REST API Running", res)
+};
+
+//Show all data
+exports.showMahasiswa = function(req,res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
